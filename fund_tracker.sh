2da9c5ff1377d280bb0599bd424a7db7a2a068c4
gsteeds <steeds.g@gmail.com>
2022-05-05 00:07:47 -0500

    Add fund_tracker.sh.

---
+++ fund_tracker.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# Activate virtual environment.
+source ./venv/bin/activate
+
+# Run program and push standard output to text file & errors to another text file..
+python fund_tracker.py -i
